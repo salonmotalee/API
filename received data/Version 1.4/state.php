91bb6e70e7a850910d7e99c394e9a078b378d545
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-08-07 12:32:26 +0330

    Update state.php

--- received data/Version 1.4/state.php
+++ received data/Version 1.4/state.php
@@ -1,1 +1,16 @@
+<?php
+include("lib/db.php");
 
+if(isset($_GET['token'])){//token is sent...
+  $token = $_GET['token'];
+  if($token == ""){//legal token...
+    
+  }else{//ilegall token...
+    
+  }
+
+}else{
+  //I can ot found your token!
+}
+
+?>
